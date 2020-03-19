34473f1044aaf88561e0271b116d7e33bd13b2aa
MasonChance <mason.fryberger@gmail.com>
2020-03-18 23:59:23 -0700

    Js loaded for assignment 06

--- gui.js
+++ gui.js
@@ -1,7 +1,21 @@
 
-var name =;
-var dob =;
-var email=;
-var singUp=;
-var errorMsg =;
+var name = prompt();
+var message;
+var timeNow = today.getHours();
+var sorry;
+
+if name(){
+    message ='hello'name;
+}
+else if{
+    message ='lets get acquainted';
+}
+
+    newFunction();
+
+
+
+function newFunction() {
+    document.write(<h2>'hello''name'</h2>, h2 >);
+}
 /// prompt, confirm, alert, document.write method. 
