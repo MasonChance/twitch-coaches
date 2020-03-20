ff11f52f8580936efe069187a10a2a429c2f09ce
MasonChance <mason.fryberger@gmail.com>
2020-03-20 00:09:36 -0700

    more script frustration

--- gui.js
+++ gui.js
@@ -1,33 +1,37 @@
-// var name;
-// var today = new Date();
-// var gameTime = today.getHours();
-// var message;
-// var greeting = prompt("Hi, let's get accquainted");
-// var takeTwo = prompt();
-// var error = prompt("you didn't tell me your name");
+var name;
+var today = new Date();
+var gameTime = today.getHours();
+var message;
+var greeting = prompt("Hi, let's get accquainted");
+//var takeTwo = prompt();
+var error = prompt("you didn't tell me your name");
 
-// if(greeting = null){
-//     name = error;
+if(greeting = null){
+    error;
      
-// }else if(greeting = false){
-//    name = error;
-// }else{
-//     message = 'Hi' + name
-//else if(error = true){
-//     name = error;
-//     message = 'hi'+ name + gameTime+ 'is always game time!';
-// }else{
-//     name = ;
-//     message = 'hi'+ name + gameTime+ 'is always game time!';
+ }else if(error = null){
 
-// }
+    message = 'Welcome' + gameTime + 'is always game time';
 
+ }else{
+     message = 'Hi' + greeting + gameTime + 'is always game time';
+
+ }
   
 ///////////// above code is close to working but requires a repeat assignment of variable and when the variable is reassigned the value of the prompt variable it does accuire the value of the data entered but rather the value of "true" which is what prints.
 
-//document.write(message);
+document.write(message);
 
 
+//var today = new Date();
+//var time = today.getHours();
+//var gameTime = twitch();
+
+//function twitch(
+    //var clock = sum(time,today.getMinutes);
+    //var greeting = 'hkjhdfkjfahdjk';
+    
+)
 
 
 
