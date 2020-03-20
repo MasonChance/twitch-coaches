9711d407b5a54bcb443570ee3991c019794f55b4
MasonChance <mason.fryberger@gmail.com>
2020-03-19 22:24:52 -0700

    worked on original js code. commented out to work on other assignments

--- gui.js
+++ gui.js
@@ -1,15 +1,40 @@
-//var name = prompt('hi lets get aquainted');
-var today = new Date();
-var gameTime = today.getHours();
+// var name;
+// var today = new Date();
+// var gameTime = today.getHours();
+// var message;
+// var greeting = prompt("Hi, let's get accquainted");
+// var takeTwo = prompt();
+// var error = prompt("you didn't tell me your name");
+
+// if(greeting = null){
+//     name = error;
+     
+// }else if(greeting = false){
+//    name = error;
+// }else{
+//     message = 'Hi' + name
+//else if(error = true){
+//     name = error;
+//     message = 'hi'+ name + gameTime+ 'is always game time!';
+// }else{
+//     name = ;
+//     message = 'hi'+ name + gameTime+ 'is always game time!';
+
+// }
+
+  
+///////////// above code is close to working but requires a repeat assignment of variable and when the variable is reassigned the value of the prompt variable it does accuire the value of the data entered but rather the value of "true" which is what prints.
+
+//document.write(message);
 
 
-// if (name === st){
-//     message = 'hi'+ name + gameTime + 'is always game time!';
-// use a <while> loop to validate for empty string
-// use a if-else to evealuate for greeting change cases
-//  };
 
 
-console.log(getHours);
+
+
+
+
+
+
 
 
