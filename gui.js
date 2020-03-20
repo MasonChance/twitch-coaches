726ebce72eeefab529059d5c8f490001e07ac245
MasonChance <mason.fryberger@gmail.com>
2020-03-20 08:43:58 -0700

    nested 'if' sequence to run prompts

--- gui.js
+++ gui.js
@@ -1,22 +1,25 @@
-var name;
+//var min = today.getMinutes();
+
 var today = new Date();
 var gameTime = today.getHours();
 var message;
 var greeting = prompt("Hi, let's get accquainted");
 //var takeTwo = prompt();
-var error = prompt("you didn't tell me your name");
+let error;
 
-if(greeting = null){
-    error;
-     
- }else if(error = null){
+if(greeting == null || greeting == false){
+    let error = prompt("you didn't tell me your name");
+     if(error == null || error == false){
+         let error = "welcome";
+         message = error + gameTime + 'is always game time';
+     }else{
+        message = error + gameTime + 'is always game time';
+     }
+}else{
+   
+   message = greeting + gameTime + "is always game time";
 
-    message = 'Welcome' + gameTime + 'is always game time';
-
- }else{
-     message = 'Hi' + greeting + gameTime + 'is always game time';
-
- }
+}
   
 ///////////// above code is close to working but requires a repeat assignment of variable and when the variable is reassigned the value of the prompt variable it does accuire the value of the data entered but rather the value of "true" which is what prints.
 
@@ -30,8 +33,8 @@ document.write(message);
 //function twitch(
     //var clock = sum(time,today.getMinutes);
     //var greeting = 'hkjhdfkjfahdjk';
-    
-)
+
+
 
 
 
